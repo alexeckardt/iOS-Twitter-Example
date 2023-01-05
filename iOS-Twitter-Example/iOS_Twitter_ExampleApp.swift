e752a165442770234d33ba964d1c49eef45cfffc
Alex Eckardt <realalexeck@gmail.com>
2023-01-05 18:17:51 -0500

    Initial Commit

---
+++ iOS-Twitter-Example/iOS_Twitter_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Twitter_ExampleApp.swift
+//  iOS-Twitter-Example
+//
+//  Created by Anita Eckardt on 2023-01-05.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Twitter_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
