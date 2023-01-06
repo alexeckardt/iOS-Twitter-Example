95ebec9ad51213e5b0b043bf8c16553d3fca0855
Alex Eckardt <realalexeck@gmail.com>
2023-01-06 14:53:13 -0500

    SideBar Toggeling

--- iOS-Twitter-Example/ContentView.swift
+++ iOS-Twitter-Example/ContentView.swift
@@ -8,19 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var sidebarVisible = true;
+    private var sidebarWidth : CGFloat = 300;
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack(alignment: .topLeading) {
+            
+            //Show Bkg
+            MainTabView()
+                .navigationBarHidden(sidebarVisible)
+            
+            //Darken Bkg
+            if (sidebarVisible) {
+                ZStack {
+                    Color(.black).opacity(sidebarVisible ? 0.25 : 0.0)
+                }
+                .ignoresSafeArea()
+                //If Tap On Dark, Dismiss the side menu
+                .onTapGesture {
+                    withAnimation(.easeInOut) {
+                        sidebarVisible = false;
+                    }
+                }
+            }
+            
+            //
+            SideMenuView()
+                .frame(width:sidebarWidth)
+                .background(Color.red)
+                .offset(x: sidebarVisible ? 0 : -sidebarWidth)
+                
         }
-        .padding()
+        //
+        //Navigation Bar Info
+        //
+        .navigationTitle("Home")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    
+                    //Show and Hide
+                    withAnimation(.easeInOut) {
+                        sidebarVisible.toggle()
+                    }
+                    
+                } label: {
+                    
+                    Circle()
+                        .frame(width:48)
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
